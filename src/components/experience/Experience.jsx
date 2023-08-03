ebbf5b83b5c72c42679cecbbce1248473a2e659d
Ricky <xproxskyx@gmail.com>
2023-08-02 23:59:13 -0400

    Added icons for experience and adjusted icon sizes

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "./experience.css"
 import UPS from "../../assets/ups.png"
+import UOFL from "../../assets/uofl.png"
+import ANDROID from "../../assets/android.png"
+import JAVS from "../../assets/javs.png"
 
 const Experience = () => {
   return (
@@ -38,7 +41,10 @@ const Experience = () => {
               <span className="experience__line"></span>
             </div>
             <div>
+            <div className="skills__data">
+            <img src={ANDROID} className="icon__size_experience"/>
             <h3 className="experience__title">Self-Employed</h3>
+            </div>
             <span className="experience__subtitle">Android Software Developer</span>
             <div className="experience__calendar"> 
               <i className="uil uil-calendar-alt"></i> 2018 - Present
@@ -48,7 +54,10 @@ const Experience = () => {
 
           <div className="experience__data">
           <div>
-            <h3 className="experience__title">University of Louisville</h3>
+          <div className="skills__data">
+            <img src={UOFL} className="icon__size_experience"/>
+            <h3 className="experience__title">University of Louisville</h3> 
+            </div>
             <span className="experience__subtitle">Master of Engineering - Computer Science and Engineer</span>
             <div className="experience__calendar"> 
               <i className="uil uil-calendar-alt"></i> 2019 - 2020
@@ -68,7 +77,10 @@ const Experience = () => {
               <span className="experience__line"></span>
             </div>
             <div>
+            <div className="skills__data">
+            <img src={UOFL} className="icon__size_experience"/>
             <h3 className="experience__title">University of Louisville</h3>
+            </div>
             <span className="experience__subtitle">Bachelor's degree - Computer Science and Engineer</span>
             <div className="experience__calendar"> 
               <i className="uil uil-calendar-alt"></i> 2014 - 2019
@@ -78,7 +90,10 @@ const Experience = () => {
 
           <div className="experience__data">
           <div>
+          <div className="skills__data">
+            <img src={UOFL} className="icon__size_experience"/>
             <h3 className="experience__title">University of Louisville</h3>
+            </div>
             <span className="experience__subtitle">Computer Engineer and Computer Science Tutor</span>
             <div className="experience__calendar"> 
               <i className="uil uil-calendar-alt"></i> Jan 2019 - Apr 2019
@@ -97,7 +112,10 @@ const Experience = () => {
               <span className="experience__line"></span>
             </div>
             <div>
+            <div className="skills__data">
+            <img src={JAVS} className="icon__size_experience"/>
             <h3 className="experience__title">Justice AV Solutions</h3>
+            </div>
             <span className="experience__subtitle">Software Engineer Intern</span>
             <div className="experience__calendar"> 
               <i className="uil uil-calendar-alt"></i> May 2016 - Dec 2017
